6830aae968e8d5ed7445f216a0273314c8949e8b
Adriaan Knapen <hi@aknapen.nl>
2020-03-21 15:07:26 +0100

    ✨ Set page title to the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Helmet from "react-helmet"
+
 import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
+    <Helmet title={"ClaimR - Page not found"} />
+
     <section id="one" className="main style1">
       <div className="grid-wrapper">
         <div className="col-6">
