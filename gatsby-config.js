cf704ac5b3f9e3f312a857929f693d0a81cacb33
Adriaan Knapen <hi@aknapen.nl>
2020-03-21 13:14:53 +0100

    🔧 Set the site's metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Photon",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Photon by HTML5 UP"
+    title: "ClaimR",
+    author: "Adriaan Knapen",
+    description: "Verified location-as-a-service API."
   },
   plugins: [
     'gatsby-plugin-react-helmet',
