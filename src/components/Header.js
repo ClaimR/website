5e8025632632beafc51c3dddece48ade44d9f921
Adriaan Knapen <hi@aknapen.nl>
2020-03-22 15:12:39 +0100

    ✨ Add the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,11 @@ class Header extends React.Component {
         <div className="inner">
           <span className="icon major fa-cloud"></span>
           <h1>
-            Hi, I'm <strong>ClaimR</strong>, with me on your side
-            <br />
-            it will be a breeze to verify your user's location.
+            Hi, I'm <strong>ClaimR</strong>, the secure, convenient
+            <br /> and trustworthy location verification service.
           </h1>
           <p>
-            Accumsan feugiat mi commodo erat lorem ipsum, sed magna
-            <br />
-            lobortis feugiat sapien sed etiam volutpat accumsan.
+            With me on your side, verify your user's location will be a breeze.
           </p>
           <ul className="actions">
             <li>
