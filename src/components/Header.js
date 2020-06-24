4e6893c82e06f984c276aa5e8b1151ab4d49776b
Adriaan Knapen <hi@aknapen.nl>
2020-06-24 10:52:48 +0200

    style: use arrow based react components instead of classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,29 @@
 import React from 'react'
 
-class Header extends React.Component {
-  render() {
-    return (
-      <section id="header">
-        <div className="inner">
-          <span className="icon major fa-cloud"></span>
-          <h1>Eliminating Location Fraud. Made Easy.</h1>
-          <p>
-            With <strong>ClaimR</strong> developers can verify their users'
-            location. Fighting fraud caused by software and hardware spoofing of
-            location positioning systems.
-          </p>
-          <ul className="actions">
-            <li>
-              <a
-                href="https://youtube.com/watch?v=Nn2FN5SepGA"
-                className="button"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Watch demo
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-    )
-  }
-}
+const Header = () => (
+  <section id="header">
+    <div className="inner">
+      <span className="icon major fa-cloud"></span>
+      <h1>Eliminating Location Fraud. Made Easy.</h1>
+      <p>
+        With <strong>ClaimR</strong> developers can verify their users'
+        location. Fighting fraud caused by software and hardware spoofing of
+        location positioning systems.
+      </p>
+      <ul className="actions">
+        <li>
+          <a
+            href="https://youtube.com/watch?v=Nn2FN5SepGA"
+            className="button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Watch demo
+          </a>
+        </li>
+      </ul>
+    </div>
+  </section>
+)
 
 export default Header
