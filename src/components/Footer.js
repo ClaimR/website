875d03b48eaf7158da3bf36f48d1b515178e3bfb
Adriaan Knapen <hi@aknapen.nl>
2020-06-24 10:52:48 +0200

    style: use arrow based react components instead of classes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,38 @@
 import React from 'react'
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <section id="footer">
-        <ul className="icons">
-          {/* <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li> */}
-          {/* <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li> */}
-          {/* <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li> */}
-          <li>
-            <a
-              href="https://github.com/ClaimR"
-              className="icon alt fa-github"
-              alt="ClaimR's Github"
-            >
-              <span className="label">GitHub</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/channel/UCZRWpBaWAeLP6uYLE0kDbRg"
-              alt="ClaimR's Youtube Channel"
-              className="icon alt fa-youtube-play"
-            >
-              <span className="label">Youtube</span>
-            </a>
-          </li>
-          {/* <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li> */}
-        </ul>
-        <ul className="copyright">
-          <li>&copy; ClaimR 2020</li>
-          <li>
-            Design by <a href="//html5up.net">HTML5 UP</a>
-          </li>
-        </ul>
-      </section>
-    )
-  }
-}
+const Footer = () => (
+  <section id="footer">
+    <ul className="icons">
+      {/* <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li> */}
+      {/* <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li> */}
+      {/* <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li> */}
+      <li>
+        <a
+          href="https://github.com/ClaimR"
+          className="icon alt fa-github"
+          alt="ClaimR's Github"
+        >
+          <span className="label">GitHub</span>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.youtube.com/channel/UCZRWpBaWAeLP6uYLE0kDbRg"
+          alt="ClaimR's Youtube Channel"
+          className="icon alt fa-youtube-play"
+        >
+          <span className="label">Youtube</span>
+        </a>
+      </li>
+      {/* <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li> */}
+    </ul>
+    <ul className="copyright">
+      <li>&copy; ClaimR 2020</li>
+      <li>
+        Design by <a href="//html5up.net">HTML5 UP</a>
+      </li>
+    </ul>
+  </section>
+)
 
 export default Footer
